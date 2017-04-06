91d01cd94b44353f968b5895bfd17e15b55b3b8c
kyfstll <liulin_sh@hotmail.com>
2017-04-06 15:30:46 -0700

    Update LinkedListLoop.java
    update how to loop for linkedlist, and comments the error for first version compiled in IDE(Intellij)

--- TIPS/LinkedList/LinkedListLoop.java
+++ TIPS/LinkedList/LinkedListLoop.java
@@ -1,10 +1,31 @@
-*package LinkedList*
+package LinkedList;
 puclic class LinkedListLoop{
   public static void main(String[] args){
+    //lots of compile error for this first version
+    //missing import java.util.*
+    //some lt wrongly type as "ls";
+    //hasNext() type as has.Next()
+    //line20, it should be "it.next()", lt is linkedlist, do not have a menthod "next()"
     LinkedList lt = new LinkedList();
     lt.add(1);
     lt.add(2);
-    
-  }
-  
+    lt.add(3);
+    System.out.println("Loop the LinkedList by ===> for");
+      for(int i=0;i<ls.size();i++){
+        System.out.print(lt.get(i) + " ");  
+      }
+    System.out.println("Loop the LinkedList by ===> iteartor");
+      Iteartor it = lt.iteartor();
+      while(it.has.Next()){
+        System.out.print(lt.next() + " ");
+      }
+    System.out.println("Loop the LinkedList by ===> while");
+      int nums=0;
+      while(ls.size()>nums){
+        System.out.print(lt.get(nums) + " ");
+        nums++;
+      }
+    System.out.println("Loop the LinkedList by ===>java8");
+      lt.forEach(System.out::println);//need check this more
+  }  
 }
