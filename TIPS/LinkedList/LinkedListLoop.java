1eae944328bc3ad3dbadbaa1e853780165eab61b
kyfstll <liulin_sh@hotmail.com>
2017-04-06 14:44:07 -0700

    Create LinkedListLoop.java

---
+++ TIPS/LinkedList/LinkedListLoop.java
@@ -0,0 +1,10 @@
+*package LinkedList*
+puclic class LinkedListLoop{
+  public static void main(String[] args){
+    LinkedList lt = new LinkedList();
+    lt.add(1);
+    lt.add(2);
+    
+  }
+  
+}
